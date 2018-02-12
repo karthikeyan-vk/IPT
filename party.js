6dc481a4413e2a6d12bc468b74bb3f06b07e932e
dineshmoorthy95 <36147337+dineshmoorthy95@users.noreply.github.com>
2018-02-12 18:46:21 +0530

    dinesh
    party time

--- party.js
+++ party.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    $(".header").addClass("test");
-});
+    $(".menuLinks").unbind('click').bind('click', function(event)
+    {
+        var contId = $(this).attr("ref");
+        $(".welcomeCont").addClass("hide");
+        $(".mainTabs").addClass("hide");
+        $("#"+ contId).removeClass("hide");
+    });
+})
